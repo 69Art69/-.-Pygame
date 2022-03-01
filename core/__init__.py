13024122ff92958edf2bf2e70a4b0b66b246a296
69Art69 <nasib.weaff69@gmail.com>
2022-03-01 19:51:44 +0500

    Pygame

---
+++ core/__init__.py
@@ -0,0 +1,130 @@
+import random
+from core.sound_effects import play_collision, play_apple_bite
+
+
+class Snake:
+
+    class Cell:
+        def __init__(self, coordinate, next_=None, prev_=None):
+            self.coordinate = coordinate
+            self.next = next_
+            self.prev = prev_
+
+    def __init__(self, box_size, init_len=5):
+        if init_len < 2:
+            raise Exception("Snake must be at least of size 2")
+        self.box_size = box_size
+        self.food_location = [None, None]
+        self.head = self.Cell(coordinate=[0, init_len - 1])
+
+        _iter_cell = self.head
+
+        self.tail = None
+        for i in range(init_len - 2, -1, -1):
+            _iter_cell.next = self.Cell(coordinate=[0, i])
+            _iter_cell.next.prev = _iter_cell
+            _iter_cell = _iter_cell.next
+
+        self.tail = _iter_cell
+
+        self.is_alive = True  
+
+    def __contains__(self, item: list):
+        cell = self.head.next
+        while cell is not None:
+            if item == cell.coordinate:
+                return True
+
+            cell = cell.next
+        return False
+
+    def pop(self):
+        removing_cell = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        removing_cell.prev = None
+        return removing_cell.coordinate
+
+    def prepend(self, coordinate):
+        self.head.prev = self.Cell(coordinate)
+        self.head.prev.next = self.head
+        self.head = self.head.prev
+
+    def get_direction_facing(self):
+        snake_coordinate = self.head.coordinate
+        snake_neck_coordinate = self.head.next.coordinate
+
+        if snake_coordinate[0] == snake_neck_coordinate[0]: 
+            if snake_coordinate[1] > snake_neck_coordinate[1]:  
+                return 3
+            else: 
+                return 0
+        else: 
+            if snake_coordinate[0] > snake_neck_coordinate[0]: 
+                return 2
+            else: 
+                return 1
+
+    def move(self, direction):
+        if direction == 'u':
+            next_coordinate = [self.head.coordinate[0], self.head.coordinate[1] - 1]
+        elif direction == 'd':
+            next_coordinate = [self.head.coordinate[0], self.head.coordinate[1] + 1]
+        elif direction == 'l':
+            next_coordinate = [self.head.coordinate[0] - 1, self.head.coordinate[1]]
+        elif direction == 'r':
+            next_coordinate = [self.head.coordinate[0] + 1, self.head.coordinate[1]]
+        else:
+            raise Exception("Invalid Direction")
+
+        if self.head.next.coordinate == next_coordinate:
+            print('Trying to move backward')
+            return 0  
+
+        if -1 in next_coordinate or max(next_coordinate) >= self.box_size:
+            self.is_alive = False
+            play_collision()
+            print("Collision with wall!")
+            return -1  
+
+        if next_coordinate in self:
+            self.is_alive = False
+            print("Bit Myself!")
+            return -2  
+
+        if next_coordinate != self.food_location:
+            self.pop()
+        else:
+            play_apple_bite()
+            self.food_location[0] = None
+            self.food_location[1] = None
+
+        self.prepend(next_coordinate)
+
+    def iter_through_body_cells(self):
+        curr_cell = self.head.next
+        while curr_cell:
+            yield curr_cell.coordinate
+            curr_cell = curr_cell.next
+
+
+class SnakeBoard:
+    DIRECTIONS = ['l', 'r', 'u', 'd']
+    score = 0
+
+    def __init__(self, board_size, snake_init_len):
+        self.snake = Snake(box_size=board_size, init_len=snake_init_len)
+        self.SIZE = board_size
+        self.food_coordinate = self.snake.food_location
+        self.initialize_food()
+
+    def initialize_food(self):
+        self.food_coordinate = self.snake.food_location = [random.randint(0, self.SIZE - 1), random.randint(0, self.SIZE - 1)]
+        check = True
+        while check:
+            check = False
+            for coord in self.snake.iter_through_body_cells():
+                if coord[0] == self.food_coordinate[0] and coord[1] == self.food_coordinate[1]:
+                    self.food_coordinate = self.snake.food_location = [random.randint(0, self.SIZE - 1), random.randint(0, self.SIZE - 1)]
+                    check = True
+                    break
---
+++ main.py
@@ -0,0 +1,181 @@
+import pygame
+from core import SnakeBoard
+
+
+WINDOW_SIZE = 500
+WINDOW_MARGIN = (40, 30, 20, 10)
+GRID_COLOR = (100, 100, 100)
+GRID_SIZE = 20
+FLOOR_COLOR_1 = (60, 155, 0)
+FLOOR_COLOR_2 = (60, 120, 0)
+SNAKE_INIT_LEN = 12
+SNAKE_BODY_COLOR = (255, 255, 0)
+SNAKE_HEAD_PATH = 'res/snake-head.png'
+FOOD_COLOR = (255, 0, 0)
+FOOD_SIZE = GRID_SIZE // 2 - 3
+
+
+angles = {
+    1: 0,
+    0: 270,
+    3: 90,
+    2: 180
+}
+
+pygame.init()
+for x in range(WINDOW_SIZE//GRID_SIZE):
+    pygame.draw.line(screen, GRID_COLOR, (x * GRID_SIZE, 0), (x * GRID_SIZE, WINDOW_SIZE))
+
+pygame.display.flip()
+
+screen = pygame.display.set_mode([WINDOW_SIZE, WINDOW_SIZE + 50])
+clock = pygame.time.Clock()
+snake_head = pygame.image.load(SNAKE_HEAD_PATH)
+snake_head = pygame.transform.scale(snake_head, (GRID_SIZE + 10, GRID_SIZE + 10))
+
+screen.fill((0, 0, 0))
+pygame.display.set_caption('Snake Game')
+
+score_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+
+def draw_snake(snake_board):
+    for coord in snake_board.snake.iter_through_body_cells():
+        pygame.draw.rect(screen,
+                         SNAKE_BODY_COLOR,
+                         [
+                             coord[0] * GRID_SIZE,
+                             coord[1] * GRID_SIZE,
+                             GRID_SIZE,
+                             GRID_SIZE
+                         ])
+
+    coord = snake_board.snake.head.coordinate
+    snake_head_tmp = pygame.transform.rotate(snake_head, angles[snake_board.snake.get_direction_facing()])
+    screen.blit(snake_head_tmp, (
+        coord[0] * GRID_SIZE - 5,
+        coord[1] * GRID_SIZE - 5,
+    ))
+
+
+def draw_food(snake_board):
+    if snake_board.food_coordinate[0] is None:
+        return False
+
+    pygame.draw.circle(screen,
+                       FOOD_COLOR,
+                       [
+                           snake_board.food_coordinate[0] * GRID_SIZE + (GRID_SIZE // 2),
+                           snake_board.food_coordinate[1] * GRID_SIZE + (GRID_SIZE // 2)
+                       ],
+                       FOOD_SIZE)
+
+
+
+def main():
+    session_score = 0
+    play_button_size = (100, 50)
+    screen.fill((0, 0, 0))
+
+    menu_font = pygame.font.SysFont('Comic Sans MS', 50)
+    prev_score_font = pygame.font.SysFont('Comic Sans MS', 40)
+
+    def show_menu():
+        screen.fill((0, 0, 0))
+        menu_play_text = menu_font.render("Play!", True, (0, 0, 0))
+        prev_score_text = prev_score_font.render("Score: "+str(session_score), False, (255, 255, 255))
+        pygame.draw.rect(screen,
+                         (100, 255, 0),
+                         [
+                             (WINDOW_SIZE / 2) - (play_button_size[0] / 2),
+                             (WINDOW_SIZE / 2) - (play_button_size[1] / 2),
+                             play_button_size[0],
+                             play_button_size[1]
+                         ])
+        screen.blit(menu_play_text, [
+            (WINDOW_SIZE / 2) - (play_button_size[0] / 2) + 10,
+            (WINDOW_SIZE / 2) - (play_button_size[1] / 2) + 10
+        ])
+
+        screen.blit(prev_score_text, [
+            (WINDOW_SIZE / 2) - (play_button_size[0] / 2),
+            (WINDOW_SIZE / 2) - (play_button_size[1] / 2) + 60
+        ])
+
+    show_menu()
+    while True:
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                pygame.quit()
+                return 0
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+
+                if (WINDOW_SIZE / 2) - (play_button_size[0] / 2) <= mouse_pos[0] <= (WINDOW_SIZE / 2) +\
+                        (play_button_size[0] / 2) and (WINDOW_SIZE / 2) - \
+                        (play_button_size[1] / 2) <= mouse_pos[1] <= (WINDOW_SIZE / 2) + (play_button_size[1] / 2):
+                    session_score = mainloop()
+
+                    show_menu()
+
+
+def mainloop():
+    current_heading_direction = 'r'
+    prev_heading_direction = current_heading_direction
+    sb = SnakeBoard(board_size=WINDOW_SIZE // GRID_SIZE, snake_init_len=SNAKE_INIT_LEN)
+
+    while True:
+        if not sb.snake.is_alive:
+            return sb.score
+
+        if sb.food_coordinate[0] is None:
+            sb.initialize_food()
+            sb.score += 10
+
+        screen.fill((0, 0, 0))
+        for x in range(WINDOW_SIZE//GRID_SIZE):
+            for y in range(WINDOW_SIZE//GRID_SIZE):
+                if (x + y) % 2:
+                    color = FLOOR_COLOR_1
+                else:
+                    color = FLOOR_COLOR_2
+                pygame.draw.rect(screen,
+                                 color,
+                                 [
+                                     x * GRID_SIZE,
+                                     y * GRID_SIZE,
+                                     GRID_SIZE,
+                                     GRID_SIZE
+                                 ])
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                return sb.score
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    current_heading_direction = 'u'
+                elif event.key == pygame.K_DOWN:
+                    current_heading_direction = 'd'
+                elif event.key == pygame.K_LEFT:
+                    current_heading_direction = 'l'
+                elif event.key == pygame.K_RIGHT:
+                    current_heading_direction = 'r'
+
+        score_board = score_font.render("Score: " + str(sb.score), True, (255, 255, 255))
+        screen.blit(score_board, (15, WINDOW_SIZE + 15))
+
+        if sb.snake.move(current_heading_direction) == 0:  
+            current_heading_direction = prev_heading_direction
+        else:
+            prev_heading_direction = current_heading_direction
+
+        draw_snake(sb)
+        draw_food(sb)
+        clock.tick(10)  
+        pygame.display.flip() 
+
+
+if __name__ == '__main__':
+    main()
